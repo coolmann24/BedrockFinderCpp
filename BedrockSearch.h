80c9ddc9bf776fa2c0f6f7336097f0003a84d08c
coolmann24 <coolmannn24@gmail.com>
2019-08-14 21:27:32 -0500

    Threaded search

---
+++ BedrockSearch.h
@@ -0,0 +1,49 @@
+#include <vector>
+#include <set>
+#include <tuple>
+#include <functional>
+#include <iostream>
+#include <thread>
+
+enum FormType : bool{
+	DEFAULT=false,
+	BEDROCK=true
+};
+
+using BedrockGenFunc = std::function<bool(int, int, int)>;
+using Formation = std::set<std::tuple<int, int, int, FormType>>;
+using Bounds = std::tuple<int, int, int, int, int, int>;//xmin, xmax, zmin, zmax, ymin, ymax
+
+void search(Bounds bounds, Formation form, BedrockGenFunc func)
+{
+	for(int y = std::get<4>(bounds); y <= std::get<5>(bounds); y++)
+		for (int x = std::get<0>(bounds); x <= std::get<1>(bounds); x++)
+			for (int z = std::get<2>(bounds); z <= std::get<3>(bounds); z++)
+			{
+				bool found = true;
+
+				for (const auto& point : form)
+				{
+					found = std::get<3>(point) == func(std::get<0>(point) + x, std::get<1>(point) + y, std::get<2>(point) + z);
+					if (!found)break;
+				}
+
+				if (found)std::cout << "X: " << x << " Z: " << z << std::endl;
+			}
+}
+
+void threadedSearch(Bounds bounds, Formation form, BedrockGenFunc func, int num_threads)
+{
+	if (num_threads <= 0)return;
+
+	int width = std::abs(std::get<1>(bounds) - std::get<0>(bounds));
+	std::vector<std::thread> threads;
+	for (int i = 0; i < num_threads - 1; i++)
+	{
+		threads.emplace_back(search, std::make_tuple(std::get<0>(bounds) + i * (width / num_threads), std::get<0>(bounds) + (i + 1) * (width / num_threads) - 1, std::get<2>(bounds), std::get<3>(bounds), std::get<4>(bounds), std::get<5>(bounds)), form, func);
+	}
+	threads.emplace_back(search, std::make_tuple(std::get<0>(bounds) + (num_threads-1) * (width / num_threads), std::get<1>(bounds), std::get<2>(bounds), std::get<3>(bounds), std::get<4>(bounds), std::get<5>(bounds)), form, func);
+
+	for (size_t i = 0; i < threads.size(); i++)
+		threads[i].join();
+}
--- Main.cpp
+++ Main.cpp
@@ -2,173 +2,30 @@
 #include <iostream>
 #include <set>
 #include <tuple>
-#include <array>
 #include "PrecomputedRandAdvance.h"
-
-inline bool bedrockOverworld112(int x, int y, int z);
-bool bedrockOverworld113(int x, int y, int z);
-bool bedrockNether113(int x, int y, int z);
-bool bedrockOverworld114(int x, int y, int z);
-bool bedrockNether114(int x, int y, int z);
+#include "BedrockGen.h"
+#include "BedrockSearch.h"
 
 int main()
 {
-	std::set<std::tuple<int, int, int>> bedrocks;
+	std::set<std::tuple<int, int, int, FormType>> bedrocks;
 
-	bedrocks.insert(std::make_tuple(0, 0, 0));
-	bedrocks.insert(std::make_tuple(0, 0, 1));
-	bedrocks.insert(std::make_tuple(0, 0, 2));
+	bedrocks.insert(std::make_tuple(0, 0, 0, FormType::BEDROCK));
+	bedrocks.insert(std::make_tuple(0, 0, 1, FormType::BEDROCK));
+	bedrocks.insert(std::make_tuple(0, 0, 2, FormType::BEDROCK));
 
-	bedrocks.insert(std::make_tuple(1, 0, 0));
-	bedrocks.insert(std::make_tuple(1, 0, 1));
-	bedrocks.insert(std::make_tuple(1, 0, 2));
+	bedrocks.insert(std::make_tuple(1, 0, 0, FormType::BEDROCK));
+	bedrocks.insert(std::make_tuple(1, 0, 1, FormType::BEDROCK));
+	bedrocks.insert(std::make_tuple(1, 0, 2, FormType::BEDROCK));
 
-	bedrocks.insert(std::make_tuple(2, 0, 0));
-	bedrocks.insert(std::make_tuple(2, 0, 1));
-	bedrocks.insert(std::make_tuple(2, 0, 2));
+	bedrocks.insert(std::make_tuple(2, 0, 0, FormType::BEDROCK));
+	bedrocks.insert(std::make_tuple(2, 0, 1, FormType::BEDROCK));
+	bedrocks.insert(std::make_tuple(2, 0, 2, FormType::BEDROCK));
+	//check accuracy of threaded search
 
-	/*bedrocks.insert(std::make_tuple(0, 0, 0));
+	threadedSearch(std::make_tuple(-10000, 10000, -10000, 10000, 4, 4), bedrocks, bedrockOverworld114, 1);
 
-	bedrocks.insert(std::make_tuple(0, 0, 1));
-	bedrocks.insert(std::make_tuple(0, 0, 2));
-	bedrocks.insert(std::make_tuple(-1, 0, 2));
-	//bedrocks.insert(std::make_tuple(2, 0, 2));
-
-	bedrocks.insert(std::make_tuple(0, 0, -1));
-	bedrocks.insert(std::make_tuple(0, 0, -2));
-	bedrocks.insert(std::make_tuple(1, 0, -2));
-	//bedrocks.insert(std::make_tuple(-2, 0, -2));
-
-	bedrocks.insert(std::make_tuple(1, 0, 0));
-	bedrocks.insert(std::make_tuple(2, 0, 0));
-	bedrocks.insert(std::make_tuple(2, 0, 1));
-	//bedrocks.insert(std::make_tuple(2, 0, -2));
-
-	bedrocks.insert(std::make_tuple(-1, 0, 0));
-	bedrocks.insert(std::make_tuple(-2, 0, 0));
-	bedrocks.insert(std::make_tuple(-2, 0, -1));
-	//bedrocks.insert(std::make_tuple(-2, 0, 2));*/
-
-	int minx = -1000;
-	int minz = -1000;
-
-	int maxx = 1000;
-	int maxz = 1000;
-
-	for(int x = minx; x <= maxx; x++)
-		for (int z = minz; z <= maxz; z++)
-		{
-			bool found = true;
-
-			for (const auto& point : bedrocks)
-			{
-				found = bedrockOverworld114(std::get<0>(point) + x, 4, std::get<2>(point) + z);
-				if (!found)break;
-			}
-
-			if (found)std::cout << "X: " << x << " Z: " << z << std::endl;
-		}
 
 	return 0;
 }
 
-inline int64_t rawSeedFromChunk(int x, int z) //chunk coordinates
-{
-	return (((int64_t)x * (int64_t)341873128712 + (int64_t)z * (int64_t)132897987541) ^ (int64_t)0x5DEECE66D) & ((((int64_t)1 << 48) - 1));
-}
-
-inline int rand5(int64_t raw_seed, int64_t a, int64_t b)
-{
-	return (int)((((raw_seed * a + b) & (((int64_t)1 << 48) - 1)) >> 17) % ((int64_t)5));
-}
-
-inline int precompChunkIndCalcNormal(int x, int y, int z, bool nether)// y 123->126 must be 4->7
-{
-	return ((z * 16 + x) * (nether ? 8 : 4) + ((nether ? 7 : 3) - y));
-}
-
-inline void precompChunkIndCalc113(int * inds, int & count, int x, int y, int z, bool nether)// y 123->126 must be 4->7
-{
-	inds[count] = ((z * 17 + x) * (nether ? 8 : 4) + ((nether ? 7 : 3) - y));
-	count++;
-	if (x == 0)
-	{
-		inds[count] = ((z * 17 + 16) * (nether ? 8 : 4) + ((nether ? 7 : 3) - y));
-		count++;
-	}
-	if (z == 0)
-	{
-		inds[count] = ((16 * 17 + x) * (nether ? 8 : 4) + ((nether ? 7 : 3) - y));
-		count++;
-	}
-	if (x == 0 && z == 0)
-	{
-		inds[count] = ((16 * 17 + 16) * (nether ? 8 : 4) + ((nether ? 7 : 3) - y));
-		count++;
-	}	
-}
-
-inline bool bedrockOverworld112(int x, int y, int z)
-{
-	if (y == 0) return true;
-	if (y < 0 || y > 4) return false;
-
-	int precomp_ind = precompChunkIndCalcNormal(x & 15, y - 1, z & 15, false);
-
-	return rand5(rawSeedFromChunk(x < 0 ? -1 + ((x + 1) / 16) : x / 16, z < 0 ? -1 + ((z + 1) / 16) : z / 16), A_OW_112[precomp_ind], B_OW_112[precomp_ind]) >= y;
-}
-
-bool bedrockOverworld113(int x, int y, int z)
-{
-	if (y == 0) return true;
-	if (y < 0 || y > 4) return false;
-
-	int inds[4];
-	int count = 0;
-	precompChunkIndCalc113(inds, count, x & 15, y - 1, z & 15, false);
-
-	bool found = false;
-
-	for(int i = 0; i < count; i++)
-		found = found || rand5(rawSeedFromChunk(x < 0 ? -1 + ((x + 1) / 16) : x / 16, z < 0 ? -1 + ((z + 1) / 16) : z / 16), A_OW_113_114[inds[i]], B_OW_113_114[inds[i]]) >= y;
-	return found;
-}
-
-bool bedrockOverworld114(int x, int y, int z)
-{
-	if (y == 0) return true;
-	if (y < 0 || y > 4) return false;
-
-	int precomp_ind = precompChunkIndCalcNormal(x & 15, y - 1, z & 15, false);
-
-	return rand5(rawSeedFromChunk(x < 0 ? -1 + ((x + 1) / 16) : x / 16, z < 0 ? -1 + ((z + 1) / 16) : z / 16), A_OW_113_114[precomp_ind], B_OW_113_114[precomp_ind]) >= y;
-}
-
-bool bedrockNether113(int x, int y, int z)
-{
-	if (y == 0 || y == 127) return true;
-	if (y < 0 || (y > 4 && y < 123) || y > 127) return false;
-
-	int inds[4];
-	int count = 0;
-	precompChunkIndCalc113(inds, count, x & 15, y > 4 ? y - 119 : y - 1, z & 15, true);
-
-	bool found = false;
-
-	for(int i = 0; i < count; i++)
-		found = found || (y > 4 ? (4 - rand5(rawSeedFromChunk(x < 0 ? -1 + ((x + 1) / 16) : x / 16, z < 0 ? -1 + ((z + 1) / 16) : z / 16), A_N_113_114[inds[i]], B_N_113_114[inds[i]]) <= y - 123) :
-		(rand5(rawSeedFromChunk(x < 0 ? -1 + ((x + 1) / 16) : x / 16, z < 0 ? -1 + ((z + 1) / 16) : z / 16), A_N_113_114[inds[i]], B_N_113_114[inds[i]]) >= y));
-
-	return found;
-}
-
-inline bool bedrockNether114(int x, int y, int z)
-{
-	if (y == 0 || y == 127) return true;
-	if (y < 0 || (y > 4 && y < 123) || y > 127) return false;
-
-	int precomp_ind = precompChunkIndCalcNormal(x & 15, y > 4 ? y - 119 : y - 1, z & 15, true);
-
-	return y > 4 ? 4 - rand5(rawSeedFromChunk(x < 0 ? -1 + ((x + 1) / 16) : x / 16, z < 0 ? -1 + ((z + 1) / 16) : z / 16), A_N_113_114[precomp_ind], B_N_113_114[precomp_ind]) <= y - 123 :
-		rand5(rawSeedFromChunk(x < 0 ? -1 + ((x + 1) / 16) : x / 16, z < 0 ? -1 + ((z + 1) / 16) : z / 16), A_N_113_114[precomp_ind], B_N_113_114[precomp_ind]) >= y;
-}
